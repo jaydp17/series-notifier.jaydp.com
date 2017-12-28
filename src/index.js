3181aa2d4f0a70749871564f967bc4f75f4a19ad
Jaydeep Solanki <jaydp17@gmail.com>
2017-12-28 22:00:37 +0530

    setup high level design

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Component } from 'preact';
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello, World!</h1>
+			<div class="container">
+				<h1 class="title">Series Notifier</h1>
+				<h2 class="subtitle">Notifies you when <span class="text-orange">your favorite show</span> releases a new episode</h2>
 			</div>
 		);
 	}
