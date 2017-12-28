00934913e9ccf71c052dcdfd9fbb8b1c33e3ca0a
Jaydeep Solanki <jaydp17@gmail.com>
2017-12-29 02:05:39 +0530

    re-create the fb button with non fixed postion

--- src/FacebookButton.js
+++ src/FacebookButton.js
@@ -2,6 +2,7 @@ import { h } from 'preact';
 
 export default () => (
   <a href="https://m.me/snbot" class="fb-message-blue" target="_blank" rel="noopener noreferrer">
-    <i /> Message Us
+    <img src="https://i.imgur.com/hjwozo7.png" height="18" width="17" />
+    <span>Message us</span>
   </a>
 );
