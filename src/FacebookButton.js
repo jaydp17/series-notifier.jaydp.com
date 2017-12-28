243308349562d70542137cc115ace876daed70db
Jaydeep Solanki <jaydp17@gmail.com>
2017-12-28 22:27:39 +0530

    convert all componenets to statless componenets

--- src/FacebookButton.js
+++ src/FacebookButton.js
@@ -1,11 +1,7 @@
 import { Component } from "preact";
 
-export default class App extends Component {
-  render() {
-    return (
-      <a href="https://m.me/snbot" class="fb-message-blue" target="_blank">
-        <i /> Message Us
-      </a>
-    );
-  }
-}
+export default () => (
+  <a href="https://m.me/snbot" class="fb-message-blue" target="_blank">
+    <i /> Message Us
+  </a>
+);
--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import "./style";
 import { Component } from "preact";
 import FacebookButton from "./FacebookButton";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div class="container">
-        <h1 class="title">Series Notifier</h1>
-        <h2 class="subtitle">
-          Notifies you when <span class="text-orange">your favorite show</span>{" "}
-          releases a new episode
-        </h2>
-        <FacebookButton />
-      </div>
-    );
-  }
-}
+export default () => (
+  <div class="container">
+    <h1 class="title">Series Notifier</h1>
+    <h2 class="subtitle">
+      Notifies you when <span class="text-orange">your favorite show</span>{" "}
+      releases a new episode
+    </h2>
+    <FacebookButton />
+  </div>
+);
