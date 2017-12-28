584a51c46f86214459ed62f8be056a1d7b71b953
Jaydeep Solanki <jaydp17@gmail.com>
2017-12-29 00:08:34 +0530

    show screenshot on desktop

---
+++ src/PhoneImage.js
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import phoneScreenshot from '../assets/rough-screenshot.png';
+
+export default () => (
+  <div class="phone-section">
+    <img class="phone-screenshot" src={phoneScreenshot} />
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import './style';
 import { h } from 'preact';
 import FacebookButton from './FacebookButton';
+import PhoneImage from './PhoneImage';
 
 export default () => (
   <div class="container">
-    <h1 class="title">Series Notifier</h1>
-    <h2 class="subtitle">
-      Notifies you when <span class="text-orange">your favorite show</span> releases a new episode
-    </h2>
-    <FacebookButton />
+    <div class="left-pane">
+      <h1 class="title">Series Notifier</h1>
+      <h2 class="subtitle">
+        Notifies you when <span class="text-orange">your favorite show</span> releases a new episode
+      </h2>
+      <FacebookButton />
+    </div>
+    <div class="right-pane">
+      <PhoneImage />
+    </div>
   </div>
 );
