54dae82efd0d8d54da5fd8c3b38da1a99dfc0d60
Jaydeep Solanki <jaydp17@gmail.com>
2017-12-28 22:21:54 +0530

    add facebook message us button

---
+++ src/FacebookButton.js
@@ -0,0 +1,11 @@
+import { Component } from "preact";
+
+export default class App extends Component {
+  render() {
+    return (
+      <a href="https://m.me/snbot" class="fb-message-blue" target="_blank">
+        <i /> Message Us
+      </a>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import './style';
-import { Component } from 'preact';
+import "./style";
+import { Component } from "preact";
+import FacebookButton from "./FacebookButton";
 
 export default class App extends Component {
-	render() {
-		return (
-			<div class="container">
-				<h1 class="title">Series Notifier</h1>
-				<h2 class="subtitle">Notifies you when <span class="text-orange">your favorite show</span> releases a new episode</h2>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class="container">
+        <h1 class="title">Series Notifier</h1>
+        <h2 class="subtitle">
+          Notifies you when <span class="text-orange">your favorite show</span>{" "}
+          releases a new episode
+        </h2>
+        <FacebookButton />
+      </div>
+    );
+  }
 }
